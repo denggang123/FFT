b578525c83322d4fd836d8c55d811379ac20c27f
denggang123 <344896224@qq.com>
2023-05-29 00:25:01 +0800

    first commit

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main.c.o"
+  "CMakeFiles/main.dir/main.c.o.d"
+  "CMakeFiles/main.dir/src/zx_fft.c.o"
+  "CMakeFiles/main.dir/src/zx_fft.c.o.d"
+  "CMakeFiles/main.dir/src/zx_math.c.o"
+  "CMakeFiles/main.dir/src/zx_math.c.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,50 @@
+#include "zx_fft.h"
+
+#define  SAMPLE_NODES              (128)  
+COMPLEX x[SAMPLE_NODES];  
+
+static void MakeInput()  
+{  
+    int i;  
+  
+    for ( i=0;i<SAMPLE_NODES;i++ )  
+    {  
+        x[i].real = sin(PI*8*i/SAMPLE_NODES);  
+        x[i].imag  = 0.0f;  
+    }  
+}  
+  
+int main(void)  
+{  
+    int i = 0;
+
+    /* TEST FFT */
+    MakeInput();  
+    fft(x,SAMPLE_NODES);  
+
+    for (i=0; i<SAMPLE_NODES; i++) {
+        printf("%.5f %.5f\n", x[i].real, x[i].imag);
+    }
+  
+    ifft(x,SAMPLE_NODES);  
+    for (i=0; i<SAMPLE_NODES; i++) {
+        printf("%.5f %.5f\n", x[i].real, x[i].imag);
+    }
+
+    /* TEST FFT with REAL INPUTS */
+    MakeInput();  
+    fft_real(x,SAMPLE_NODES);  
+
+    for (i=0; i<SAMPLE_NODES; i++) {
+        printf("%.5f %.5f\n", x[i].real, x[i].imag);
+    }
+  
+    ifft_real(x,SAMPLE_NODES);  
+
+    for (i=0; i<SAMPLE_NODES; i++) {
+        printf("%.5f %.5f\n", x[i].real, x[i].imag);
+    }
+
+    return 0;
+}  
+
